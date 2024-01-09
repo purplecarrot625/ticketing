a3837393d7b6b7b54bf9bb04681b6f16904c3934
purplecarrot625 <97133994+purplecarrot625@users.noreply.github.com>
2024-01-09 15:45:34 -0800

    feat: onSuccess callback

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { useState } from "react";
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
       setErrors(null);
       const response = await axios[method](url, body);
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
       return response.data;
     } catch (err) {
       setErrors(
@@ -25,4 +30,5 @@ export default ({ url, method, body }) => {
 
   return { doRequest, errors };
 };
+
 
