8bc69596d4d27e9dd727a228675ed422a6195e76
purplecarrot625 <97133994+purplecarrot625@users.noreply.github.com>
2024-01-16 19:57:11 -0800

    feat: route

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,18 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  axios.get("/api/users/currentuser").catch((err) => {
-    console.log(err.message);
-  });
+  //   axios.get("/api/users/currentuser").catch((err) => {
+  //     console.log(err.message);
+  //   });
 
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios.get("/api/users/currentuser");
-
-  return response.data;
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
