58c82f41bf18acdce7d20c87b40185ef960e5a9d
purplecarrot625 <97133994+purplecarrot625@users.noreply.github.com>
2024-01-19 19:26:19 -0800

    feat: updated header

--- client/components/header.js
+++ client/components/header.js
@@ -1,3 +1,12 @@
-export default ({currentUser}) => {
-    
-}
+import Link from "next/link";
+export default ({ currentUser }) => {
+  <Link className="navbar-brand" href="/">
+    GitTix
+  </Link>;
+  <div className="d-flex justify-content-end">
+    <ul className="nav d-flex align-items-center">
+      {currentUser ? "Sign out" : "Sign in/up"}
+    </ul>
+  </div>;
+};
+
